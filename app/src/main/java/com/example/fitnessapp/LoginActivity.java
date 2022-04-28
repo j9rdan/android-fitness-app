dc0031db0fe064c62ea048f236c2b505d9e94ad9
Jordan Edoimioya <jordane910@gmail.com>
2022-04-28 18:38:05 +0100

    fixed login issue

--- app/src/main/java/com/example/fitnessapp/LoginActivity.java
+++ app/src/main/java/com/example/fitnessapp/LoginActivity.java
@@ -30,7 +30,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button loginBtn;
     private TextView forgotPassword, signUpOption;
     private ProgressBar progressBar;
-    private String email;
 
     private FirebaseAuth mAuth;
 
@@ -39,7 +38,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance(); // get firebase authentication instance
 
         username_et = findViewById(R.id.username_login_input);
         password_et = findViewById(R.id.password_login_input);
@@ -58,6 +57,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (view.getId()) {
             case R.id.loginBtn:
                 signUserIn();
+                username_et.setText("");
+                password_et.setText("");
                 break;
             case R.id.forgotPWOption:
                 resetPassword();
@@ -65,92 +66,64 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.regOption:
                 startActivity(new Intent(this, SignupActivity.class));
         }
-
     }
 
     private void resetPassword() {
+
     }
 
     private void signUserIn() {
 
-        // TO DO: STORE RETRIEVED EMAIL IN GLOBAL STRING
-
         // store inputs as strings
         String username = username_et.getText().toString().trim();
         String password = password_et.getText().toString().trim();
-        String[] email = new String[1];
-
-        // get associated email from username
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference().child("Users");
-        Query query = rootRef.orderByChild("username").equalTo(username);
-        query.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (DataSnapshot ds : task.getResult().getChildren()) {
-                        email[0] = ds.child("email").getValue(String.class);
-                        String s = "Queried email 1: " + email[0];
-                        Log.i(null, s);
-                    }
-                } else {
-                    Log.i("TAG", task.getException().getMessage()); //Don't ignore potential errors!
-                }
-            }
-        });
-
-//        rootRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-//                    if (dataSnapshot.child("username").getValue().toString().equals(username))
-//                        email = dataSnapshot.child("email").getValue().toString();
-//                }
-//                String s = "Queried email 1: " + email;
-//                Log.i(null, s);
-////                email[0] = email_db;
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
 
         // input validation
         if (username.isEmpty()) {
             username_et.setError("Enter a username");
             username_et.requestFocus();
             return;
-        }
-
-        if (password.isEmpty()) {
+        } else if (password.isEmpty()) {
             password_et.setError("Enter a password");
             password_et.requestFocus();
+            return;
         }
 
         progressBar.setVisibility(View.VISIBLE);
-        String s = "Queried email 2: " + email[0];
-        Log.i(null, s);
-        String newEmail = email[0];
-        String s2 = "new email: " + newEmail;
-        Log.i(null, s2);
-//        mAuth.signInWithEmailAndPassword(email[0], password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//            @Override
-//            public void onComplete(@NonNull Task<AuthResult> task) {
-//                if (task.isSuccessful()) {
-//                    startActivity(new Intent(LoginActivity.this, SelectProgramActivity.class));
-//                    // redirect
-//                } else {
-//                    Toast.makeText(LoginActivity.this, "Wrong credentials. Please try again", Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        });
 
+        // query Users data for child node with input username
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        Query query = rootRef.orderByChild("username").equalTo(username);
+        query.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                String email = null;
 
+                if (task.isSuccessful()) {
+                    // get associated email from username
+                    for (DataSnapshot ds : task.getResult().getChildren()) {
+                        email = ds.child("email").getValue(String.class);
+                    }
 
-    }
+                    assert email != null;
 
-    public String getUserEmail(String user) {
-        return user;
+                    // sign in with given credentials
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                startActivity(new Intent(LoginActivity.this, SelectProgramActivity.class));
+                                progressBar.setVisibility(View.GONE);
+                            } else {
+                                progressBar.setVisibility(View.GONE);
+                                Toast.makeText(LoginActivity.this, "Wrong credentials. Please try again", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                } else {
+                    Log.i("TAG", task.getException().getMessage());
+                }
+            }
+        });
     }
 }
