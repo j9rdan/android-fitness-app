0bce84097340a6b522276c19f5a2479f1879ad63
Jordan Edoimioya <jordane910@gmail.com>
2022-05-08 23:46:03 +0100

    navigation from target muscles? page

--- app/src/main/java/com/example/fitnessapp/TargetMusclesActivity.java
+++ app/src/main/java/com/example/fitnessapp/TargetMusclesActivity.java
@@ -2,13 +2,41 @@ package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TargetMusclesActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class TargetMusclesActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_yes, btn_no;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target_muscles);
+
+        // get buttons & set click listeners
+        btn_yes = findViewById(R.id.btn_yesTarget);
+        btn_no = findViewById(R.id.btn_noTarget);
+        btn_yes.setOnClickListener(this);
+        btn_no.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_yesTarget:
+                startActivity(new Intent(this, SelectMusclesActivity.class));
+                break;
+            case R.id.btn_noTarget:
+                startActivity(new Intent(this, ChooseSplitActivity.class));
+                break;
+        }
+
     }
 }
