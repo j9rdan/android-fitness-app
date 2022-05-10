da2d1f73f258ce01bdf924c7c652acf4dcf0ecd4
Jordan Edoimioya <jordane910@gmail.com>
2022-05-10 15:51:24 +0100

    added cards to home screen

--- app/src/main/java/com/example/fitnessapp/HomeActivity.java
+++ app/src/main/java/com/example/fitnessapp/HomeActivity.java
@@ -3,12 +3,29 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView split, program, nextWorkout;
+
+    // get instances of firebase auth & realtime db
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("Users").child(mAuth.getCurrentUser().getUid());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // get activity components
+        split = findViewById(R.id.userSplit);
+        program = findViewById(R.id.userProgram);
+        nextWorkout = findViewById(R.id.userNextWorkout);
     }
 }
