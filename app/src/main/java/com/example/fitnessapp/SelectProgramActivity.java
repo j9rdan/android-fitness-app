6333da2193b3fa0156c8088fc1a87d28bf2322ea
Jordan Edoimioya <jordane910@gmail.com>
2022-05-17 03:40:01 +0100

    limited program types to strength & hypertrophy for simplicity

--- app/src/main/java/com/example/fitnessapp/SelectProgramActivity.java
+++ app/src/main/java/com/example/fitnessapp/SelectProgramActivity.java
@@ -13,7 +13,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class SelectProgramActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button buildMuscle, buildStrength, buildEndurance, loseFat, homeWorkouts;
+    private Button buildMuscle, buildStrength;
 
     // get instances of firebase auth & realtime db
     private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -33,15 +33,6 @@ public class SelectProgramActivity extends AppCompatActivity implements View.OnC
 
         buildStrength = findViewById(R.id.btn_strength);
         buildStrength.setOnClickListener(this);
-
-        buildEndurance = findViewById(R.id.btn_endurance);
-        buildEndurance.setOnClickListener(this);
-
-        loseFat = findViewById(R.id.btn_fat);
-        loseFat.setOnClickListener(this);
-
-        homeWorkouts = findViewById(R.id.btn_homeWorkouts);
-        homeWorkouts.setOnClickListener(this);
     }
 
     @Override
@@ -54,18 +45,6 @@ public class SelectProgramActivity extends AppCompatActivity implements View.OnC
                 break;
             case R.id.btn_strength:
                 ref.child("program_type").setValue("strength");
-                startActivity(new Intent(SelectProgramActivity.this, AboutYouActivity.class));
-                break;
-            case R.id.btn_endurance:
-                ref.child("program_type").setValue("endurance");
-                startActivity(new Intent(SelectProgramActivity.this, AboutYouActivity.class));
-                break;
-            case R.id.btn_fat:
-                ref.child("program_type").setValue("lose weight");
-                startActivity(new Intent(SelectProgramActivity.this, AboutYouActivity.class));
-                break;
-            case R.id.btn_homeWorkouts:
-                ref.child("program_type").setValue("train at home");
                 startActivity(new Intent(SelectProgramActivity.this, AboutYouActivity.class));
                 break;
         }
