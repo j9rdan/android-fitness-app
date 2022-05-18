4d51fd6001f70eea72c6651ce5f6631590d5345f
Jordan Edoimioya <jordane910@gmail.com>
2022-05-18 14:06:42 +0100

    added webview

--- app/src/main/java/com/example/fitnessapp/WebViewActivity.java
+++ app/src/main/java/com/example/fitnessapp/WebViewActivity.java
@@ -6,18 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class WebViewActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNav;
+    private BottomNavigationView bottomNav;
+    private WebView userGuide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
+        userGuide = findViewById(R.id.guideWebView);
+        userGuide.loadUrl("file:///android_asset/guide/html/home.html");
         bottomNav = findViewById(R.id.bottomNav);
         bottomNav.setSelectedItemId(R.id.help);
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
