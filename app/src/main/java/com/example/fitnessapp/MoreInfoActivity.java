1905dbbc6ea4a44a4a4bfcf066537c4a4765e145
Jordan Edoimioya <jordane910@gmail.com>
2022-05-18 12:06:51 +0100

    home page of webview

---
+++ app/src/main/java/com/example/fitnessapp/MoreInfoActivity.java
@@ -0,0 +1,48 @@
+package com.example.fitnessapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MoreInfoActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_info);
+
+        bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setSelectedItemId(R.id.help);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                // set up navigation
+                switch(item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.insights:
+                        startActivity(new Intent(getApplicationContext(), AnalyticsActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.help:
+                        return true;
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
